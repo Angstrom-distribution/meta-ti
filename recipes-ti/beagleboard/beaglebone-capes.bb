03092005396f548c401ebbea5bf670c31614992a
Koen Kooi <koen@dominion.thruhere.net>
2012-06-07 17:40:44 +0200

    beaglebone-capes: add support for weatherstation cape
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>
    Signed-off-by: Denys Dmytriyenko <denys@ti.com>

--- recipes-ti/beagleboard/beaglebone-capes.bb
+++ recipes-ti/beagleboard/beaglebone-capes.bb
@@ -3,7 +3,7 @@ DESCRIPTION = "Userspace setup for beaglebone capes"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COREBASE}/LICENSE;md5=3f40d7994397109285ec7b81fdeb3b58"
 
-PR = "r2"
+PR = "r3"
 
 inherit allarch
 
--- recipes-ti/beagleboard/beaglebone-capes/cape.sh
+++ recipes-ti/beagleboard/beaglebone-capes/cape.sh
@@ -10,6 +10,12 @@ for eeprom in /sys/bus/i2c/devices/3-005*/eeprom ; do
 				echo "Turning on backlight for LCD4 cape"
 				i2cset -f -y 1 0x24 0x07 0x09
 				i2cset -f -y 1 0x24 0x08 0x60;;
+		"BB-BONE-WTHR-01.")
+				echo "Initializing I2C devices"
+				echo sht21 0x40 > /sys/class/i2c-adapter/i2c-3/new_device
+				echo bmp085 0x77 > /sys/class/i2c-adapter/i2c-3/new_device
+				echo tsl2550 0x39 > /sys/class/i2c-adapter/i2c-3/new_device
+				echo 1 > /sys/bus/i2c/devices/3-0039/operating_mode;;
 		*)
 				echo "unknown cape: $PARTNUMBER";;
 	esac
